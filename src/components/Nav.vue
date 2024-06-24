d941e178e34aa67a477588dc382d11e2d0137f15
csmichel <christianmichel205@gmail.com>
2024-06-24 14:19:31 -0400

    some changes, routing not working as i want it.

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,32 @@
 <template navbar>
 <ul class="nav">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Projects</a>
+    <a class="nav-link active" v-bind:href="/projects/" aria-current="page">Projects</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="/about">About Me</a>
+    <a class="nav-link" v-bind:href="/about/" @click="isClicked(about)">About Me</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="/blog">Blog</a>
+    <a class="nav-link" v-bind:href="/blog/">Blog</a>
   </li>
 </ul>
 </template>
+
+<script>
+export default {
+  name: 'navbar',
+  data() {
+    return {
+      projects: true,
+      about: true,
+      blog: true
+    };
+  },
+  methods: {
+    isClicked(elem){
+      elem = !elem;
+      return  console.log(elem);
+    }
+  }
+};
+</script>
